b3f0dca8b9c3e3365f145a281bb3c8107b2541ea
Jann Horn <jannhorn@googlemail.com>
2011-09-28 22:46:15 +0200

    examples for if and mapping

--- example/simple.js
+++ example/simple.js
@@ -1,7 +1,11 @@
 var $str_splitted_at_$delimiter = function($str, $delimiter) { return $str.split($delimiter) };
 ;
+var $str_uppercased = function($str) { return $str.toUpperCase() };
+;
 /* native literal definition */
 ;
 console.log($str_splitted_at_$delimiter("foo|bar|baz", "|"));
 if ("abc" === "def") {console.log("oh noes!");
-console.log("that can't be!")}
+console.log("that can't be!")};
+if ("abc" !== "def") {console.log("yay, the universe still works!")};
+console.log($str_splitted_at_$delimiter("foo|bar|baz", "|").map(function(it){$str_uppercased(it)}))
